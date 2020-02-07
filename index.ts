252a76cca71ececb372eef4ee035b176f7755d76
Il Harper <afanyiyu@hotmail.com>
2020-02-08 00:40:45 +0800

    feat: 🎸 Add Log Init

---
+++ index.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires, no-empty */
+import * as fs from 'fs';
+import Plugog from 'plugog';
+const { version } = require('./package.json');
+
+// Log Init
+try {
+  fs.mkdirSync('./logs');
+} catch (error) {}
+const logFile = `./logs/${new Date()
+  .toLocaleString()
+  .replace(/( |\/|:)/g, '-')}.log`;
+fs.writeFileSync(logFile, '');
+const plugog = new Plugog(fs.createWriteStream(logFile));
+const log = plugog.addPlugin('Core');
+log.i(`VkiQ v${version}`);
